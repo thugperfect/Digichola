8c996e9ad25600d2c57d6f69d3194dce13ffc8d2
thugperfect <meenamani9444@gmail.com>
2024-02-16 10:59:47 +0530

    prevent default

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./login.style.css";
 import logo from "../images/logo.png";
 import { FaEyeSlash, FaEye,FaFacebook,FaLinkedin } from "react-icons/fa";
@@ -7,9 +7,15 @@ import { FcGoogle } from "react-icons/fc";
 import { TiSocialLinkedinCircular } from "react-icons/ti";
 import { Link } from "react-router-dom";
 export default function Login() {
+    const email = useRef(null);
+    const password = useRef(null);
+
   const [showPassword, setShowPassword] = useState(false);
   const [checked,setChecked] = useState(false);
-  const formValidate = () => {};
+  const [text,setText] = useState("");
+    function formValidate(e){
+        e.preventDefault()
+    }
   return (
     <div className="cursor-default">
       <form onSubmit={formValidate}>
@@ -19,17 +25,20 @@ export default function Login() {
               <img src={logo} alt="" className="w-[50px] h-[50px] " />
               <div className="text-[40px] ">Digichola</div>
             </div>
+            <div className="h-[20px] text-red-500 flex justify-center items-center">{text}</div>
             <div className="text-2xl py-2">Login</div>
             <div className=" flex flex-col m-2">
               <label htmlFor="mail" className="text-gray-600">
                 Email
               </label>
               <input
+              ref={email}
                 type="text"
                 name=""
                 id="mail"
-                className="outline-none border border-1 text-[20px] text-gray-700 border-gray-300 h-[40px] rounded-lg "
+                className="outline-none border border-1 px-2 text-[20px] text-gray-700 border-gray-300 h-[40px] rounded-lg "
               />
+
             </div>
             <div className=" flex flex-col m-2">
               <label htmlFor="pass" className="text-gray-600">
@@ -37,10 +46,11 @@ export default function Login() {
               </label>
               <div className="flex w-full relative">
                 <input
+                ref={password}
                   type={showPassword ? "text" : "password"}
                   name=""
                   id="pass"
-                  className="outline-none border border-1 text-[20px] text-gray-700 border-gray-300 h-[40px] rounded-lg w-full"
+                  className="outline-none px-2 border border-1 text-[20px] text-gray-700 border-gray-300 h-[40px] rounded-lg w-full"
                 />
                 {showPassword ? (
                   <FaEyeSlash
@@ -54,7 +64,8 @@ export default function Login() {
                   />
                 )}
               </div>
-
+              </div>
+                    <div className="">
               <div  className=" mt-4 flex items-center gap-2 w-[200px]">
                 <div  onClick={()=>setChecked(!checked)}>
                   <div  className="w-[30px] h-[30px] border border-1 border-gray-400 rounded-md">
@@ -64,9 +75,10 @@ export default function Login() {
                 <div onClick={()=>setChecked(!checked)} className="text-gray-700">Remember me?</div>
               </div>
 
-              <button className="bg-[#ee5784] mt-2 h-[40px] rounded-lg text-white shadow-lg">LOGIN</button>
-              <div className="text-gray-700 flex justify-end cursor-pointer">Forgot Password?</div>
             </div>
+            
+            <button className="bg-[#ee5784] mt-2 h-[40px] rounded-lg text-white shadow-lg w-full">LOGIN</button>
+              <div className="text-gray-700 flex justify-end cursor-pointer">Forgot Password?</div>
 
             <div className="flex mt-2 justify-center items-center">
                 <hr className="w-[120px]"/>
@@ -79,6 +91,7 @@ export default function Login() {
              <FaFacebook className="w-[40px] h-[40px] text-blue-400"/>   
              <FaLinkedin className="w-[40px] h-[40px] text-blue-600 " />
             </div>
+
 
             <div className="flex justify-center">Need an Account? &nbsp;<Link to={"/register"} className="underline text-[#ee5784]">SIGN UP</Link></div>
           </div>
